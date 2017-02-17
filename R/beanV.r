8cb9573633510cb809652fb77d6fa0f49792ebba
datartist2015 <housheng1991@gmail.com>
2017-02-17 17:32:04 +0800

    dataV

---
+++ R/beanV.r
@@ -0,0 +1,10 @@
+﻿beanV <- function(Data,Package='beanplot'){
+	if(!Package %in% rownames(installed.packages())){
+		install.packages(Package)
+	}
+	
+	if(Package == 'beanplot'){
+		library(beanplot)
+		beanplot(Data)
+	}
+							}	
---
+++ R/missingV.r
@@ -0,0 +1,12 @@
+﻿missingV <- function(Data,Package='VIM'){
+	if(!Package %in% rownames(installed.packages())){
+		install.packages(Package)
+	}
+	
+	if(Package == 'VIM'){
+		library(VIM)
+		aggr(x = Data, prop = F, numbers = T,main = '缺失值统计')
+		dev.new()
+		matrixplot(Data, interactive = F,main = '缺失值分布')
+	}
+							}	
